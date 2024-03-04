bfc25de6f38af1a4694e2d9ba6d87065eba78d7c
lee <lee.nuckols@gmail.com>
2024-03-03 22:45:26 -0600

    Changes made to nvim-lint.lua

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -5,7 +5,7 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = {
-			paython = { "pylint" },
+			python = { "mypy", "ruff" },
 			markdown = { "vale" },
 			go = { "golangcilint" },
 			yaml = { "yamllint" },
